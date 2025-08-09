08e36bf0e015140c9cf4bba33498181ecf1e917f
Pasin0931 <pasin0931@gmail.com>
2025-08-09 19:16:28 +0700

    Improve Menu page, and add mocking data for history page

--- src/components/history.tsx
+++ src/components/history.tsx
@@ -7,18 +7,67 @@ import { motion } from "framer-motion"
 import { Card } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 
+interface dataProps {
+    id: number
+    items: string
+    points: number
+    createAt: string
+}
+
 export default function History() {
+
+    const [userData, setUserData] = useState<dataProps[] | null>(null)
+
+    const [scanTime, setScanTime] = useState(0)
+    const [totalPoints, setTotalPoints] = useState(0)
+
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        const mockingData: dataProps[] = [
+            { id: 1, items: "Plastic bag", points: 1, createAt: "50/5/2000" },
+            { id: 2, items: "Plastic bottle", points: 1.5, createAt: "50/5/2000" }
+        ]
+
+        setUserData(mockingData)
+        // setUserData(null)
+
+        const totalPoints = mockingData.reduce((sum, item) => sum + item.points, 0)
+        setTotalPoints(totalPoints)
+
+        setScanTime(mockingData.length)
+    }, [])
 
     return (
         <div className="flex flex-cols justify-center items-center h-screen">
             <motion.div
                 initial={{ y: 20, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
-                transition={{ delay: 0.05 }}>
+                transition={{ delay: 0.05 }}
+                className="w-120">
 
                 <Card className="p-10 bg-black/70 text-white border-4 border-white rounded-2xl shadow-2xl backdrop-blur-lg transition-all duration-500">
                     <h1 className="font-bold text-4xl text-center mb-2 tracking-wide">History</h1>
-                    <h2>This is your previous scan history.</h2>
+                    <h2 className="text-center">This is your previous scan history.</h2>
+                    <div>
+                        {!userData ? (
+                            <Card className="flex flex-cols justify-center items-center bg-black">
+                                <h2 className="flex flex-cols justify-center items-center text-white">No Data</h2>
+                            </Card>
+                        ) : (
+                            <div>
+                                {userData.map((usr: any) => (
+                                    <Card key={usr.id} className="gap-2 p-4 bg-transparent text-white mb-3">
+                                        <h3 className="text-bold">{usr.items}</h3>
+                                        <h4>Points : {usr.points}</h4>
+                                        <p>{usr.createAt}</p>
+                                    </Card>
+                                ))}
+                                <h5 className="flex flex-cols justify-center items-center mt-5">Accumulative point : {totalPoints}</h5>
+                                <h5 className="flex flex-cols justify-center items-center">Total scan time : {scanTime}</h5>
+                            </div>
+                        )}
+                    </div>
                     <Link href={'/'} className="w-full">
                         <Button variant={"outline"} className="hover:text-black cursor-pointer transition duration-300 bg-black w-full">
                             Return to Menu
@@ -27,6 +76,6 @@ export default function History() {
                 </Card>
 
             </motion.div>
-        </div>
+        </div >
     )
 }
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -17,8 +17,47 @@ export default function Menu() {
                 transition={{ delay: 0.05 }}>
 
                 <Card className="p-10 bg-black/70 text-white border-4 border-white rounded-2xl shadow-2xl backdrop-blur-lg transition-all duration-500">
-                    <h1 className="font-bold text-4xl text-center mb-2 tracking-wide">Menu</h1>
-                    <h2>This is where you view your points for each object you scanned !</h2>
+                    <h1 className="font-bold text-4xl text-center tracking-wide">Menu</h1>
+                    <h2 className="text-center tracking-wide mb-2">This list displays the point value for each item.</h2>
+                    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 gap-5 mb-2">
+                        <Card className="p-4 text-center gap-2 bg-transparent text-white">
+                            <h3>Aluminum Can</h3>
+                            <h4>1 Point</h4>
+                        </Card>
+                        <Card className="p-4 text-center gap-2 bg-transparent text-white">
+                            <h3>Banana Peel</h3>
+                            <h4>1 Point</h4>
+                        </Card>
+                        <Card className="p-4 text-center gap-2 bg-transparent text-white">
+                            <h3>Cardboard Box</h3>
+                            <h4>1 Point</h4>
+                        </Card>
+                        <Card className="p-4 text-center gap-2 bg-transparent text-white">
+                            <h3>Chip Bag</h3>
+                            <h4>1 Point</h4>
+                        </Card>
+                        <Card className="p-4 text-center gap-2 bg-transparent text-white">
+                            <h3>Food Wrapper</h3>
+                            <h4>1 Point</h4>
+                        </Card>
+                        <Card className="p-4 text-center gap-2 bg-transparent text-white">
+                            <h3>Glass Jar</h3>
+                            <h4>1 Point</h4>
+                        </Card>
+                        <Card className="p-4 text-center gap-2 bg-transparent text-white">
+                            <h3>Old Newspaper</h3>
+                            <h4>1 Point</h4>
+                        </Card>
+                        <Card className="p-4 text-center gap-2 bg-transparent text-white">
+                            <h3>Plastic Bottle</h3>
+                            <h4>1 Point</h4>
+                        </Card>
+                        <Card className="p-4 text-center gap-2 bg-transparent text-white">
+                            <h3>Styrofoam Cup</h3>
+                            <h4>1 Point</h4>
+                        </Card>
+                    </div>
+                    
                     <Link href={'/'} className="w-full">
                         <Button variant={"outline"} className="hover:text-black cursor-pointer transition duration-300 bg-black w-full">
                             Return to Menu
